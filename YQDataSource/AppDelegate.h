6b8f6396b78a4016a032e402db0ddf7032deb4e9
yuyedaidao <wyqpadding@gmail.com>
2018-11-05 20:58:50 +0800

    Initial Commit

---
+++ YQDataSource/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YQDataSource
+//
+//  Created by 王叶庆 on 2018/11/5.
+//  Copyright © 2018 王叶庆. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
